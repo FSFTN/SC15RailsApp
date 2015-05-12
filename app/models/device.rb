ad503d14f6187aa8b914266ab2dc05414fe8cf8b
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2015-05-12 09:49:05 +0530

    added validation for devicess

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,6 @@
 class Device < ActiveRecord::Base
- 
+  validates :email, presence: true
+  validates :device_id, presence: true
+  validates :email, uniqueness: true
+  validates :device_id, uniqueness: true
 end
