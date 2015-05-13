8cbecd70c02737a65f0dbccd735abab40557e609
Mohamed Saleem <mohsal.saleem@gmail.com>
2015-05-13 14:39:48 +0530

    Changed Datatype of phone from integer to String

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513082852) do
+ActiveRecord::Schema.define(version: 20150513090442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20150513082852) do
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "phone"
+    t.string   "phone"
   end
 
   create_table "devices", force: :cascade do |t|
